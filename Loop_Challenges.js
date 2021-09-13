784998cb011f5c167f98ab1f0c5b248cdef99762
Your Name <you@example.com>
2021-09-13 02:20:43 -0600

    Rguzma

---
+++ Loop_Challenges.js
@@ -0,0 +1,31 @@
+
+for (i=1; i <= 20; i ++ ) {
+    if (i % 2 === 1 ){
+    console.log(i) }    
+    }
+
+
+
+
+    
+for (i=100; i >= 0; i -- ) {
+        if (i % 3 === 0 ){
+        console.log(i) }    
+        }
+
+
+for (i=4; i >= -3.5;  i -= 1.5 ) {
+        console.log(i) }
+
+for (i=4; i >= -3.5;  i -= 1.5 ) {
+        console.log(i) }
+
+        let sum = 0
+        for (let i=1; i<=100; i ++) {
+            sum = sum+i
+        } console.log(sum);
+
+let product = 1
+        for (let i=1; i<=12; i ++) {
+            sum = product*i
+        } console.log(product);
